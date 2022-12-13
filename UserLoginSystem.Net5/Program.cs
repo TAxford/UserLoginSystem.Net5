2bf35de0e656d8b7b901533c21fdc0d77c771f6c
Timothy Axford <t.axford2@yahoo.com>
2022-12-12 21:44:33 -0500

    Added login portion with success or fail parameters.

--- UserLoginSystem.Net5/Program.cs
+++ UserLoginSystem.Net5/Program.cs
@@ -19,9 +19,32 @@ namespace UserLoginSystem.Net5
             userName = Console.ReadLine();
             Console.WriteLine("Please enter your password");
             password = Console.ReadLine();
+            Console.WriteLine("Registration completed");
+            Console.WriteLine("------------------------");
         }
 
 
+        public static void Login()
+        {
+            Console.WriteLine("Please enter your username");
+            if (userName == Console.ReadLine())
+            {
+                Console.WriteLine("Please enter your password");
+                if (password == Console.ReadLine())
+                {
+                    Console.WriteLine("Login successful");
+                }
+                else
+                {
+                    Console.WriteLine("Login failed, wrong password. Restart Program");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Login failed, wrong username. Restart Program");
+            }
+        }
 
     }
 }
